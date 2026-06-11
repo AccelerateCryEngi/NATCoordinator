6a71c2ccdb582f14ca85025f9de6b0d457740433
AccelerateCryEngi <113775227+AccelerateCryEngi@users.noreply.github.com>
2026-06-11 19:37:13 +0300

    Initialization

--- obj/Debug/net10.0/NATCoordinator.AssemblyInfo.cs
+++ obj/Debug/net10.0/NATCoordinator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NATCoordinator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9289bba8ae513265bff94d6ae9c9abd546513b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NATCoordinator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NATCoordinator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
